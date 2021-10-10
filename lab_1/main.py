db3f7df0c30f221804c3fe0a10a0f9304d5f8395
Filip Domanski <fdoman@student.agh.edu.pl>
2021-10-10 20:26:22 +0200

    Rozwiązanie_zadań

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,4 +1,8 @@
-def cylinder_area(r:float,h:float):
+import math
+import numpy as np
+
+
+def cylinder_area(r: float, h: float):
     """Obliczenie pola powierzchni walca. 
     Szczegółowy opis w zadaniu 1.
     
@@ -9,9 +13,13 @@ def cylinder_area(r:float,h:float):
     Returns:
     float: pole powierzchni walca 
     """
-    return None
+    if r < 0 or h < 0:
+        return np.NaN
+    else:
+        return 2 * math.pi * r**2 + 2 * math.pi * r * h
 
-def fib(n:int):
+
+def fib(n: int):
     """Obliczenie pierwszych n wyrazów ciągu Fibonnaciego. 
     Szczegółowy opis w zadaniu 3.
     
@@ -21,9 +29,23 @@ def fib(n:int):
     Returns:
     np.ndarray: wektor n pierwszych wyrazów ciągu Fibonnaciego.
     """
-    return None
+    if n < 1 or not isinstance(n, int):
+        return None
+    if n == 1:
+        return np.array([1])
 
-def matrix_calculations(a:float):
+    fib_array = np.array([[1], [1]])
+    if n == 2:
+        return fib_array
+
+    for i in range(2, n):
+        fib_array = np.append(fib_array, fib_array[i - 2] + fib_array[i - 1])
+    fib_nd_array = np.reshape(fib_array, (1, n))
+
+    return fib_nd_array
+
+
+def matrix_calculations(a: float):
     """Funkcja zwraca wartości obliczeń na macierzy stworzonej 
     na podstawie parametru a.  
     Szczegółowy opis w zadaniu 4.
@@ -35,11 +57,21 @@ def matrix_calculations(a:float):
     touple: krotka zawierająca wyniki obliczeń 
     (Minv, Mt, Mdet) - opis parametrów w zadaniu 4.
     """
-    return None
+    matrix = np.array([[a, 1, -a], [0, 1, 1], [-a, a, 1]])
 
-def custom_matrix(m:int, n:int):
+    mt = matrix.transpose()
+    mdet = np.linalg.det(matrix)
+    if mdet == 0:
+        minv = np.NaN
+    else:
+        minv = np.linalg.inv(matrix)
+
+    return minv, mt, mdet
+
+
+def custom_matrix(m: int, n: int):
     """Funkcja zwraca macierz o wymiarze mxn zgodnie 
-    z opisem zadania 7.  
+    z opisem zadania 6.
     
     Parameters:
     m (int): ilość wierszy macierzy
@@ -48,4 +80,15 @@ def custom_matrix(m:int, n:int):
     Returns:
     np.ndarray: macierz zgodna z opisem z zadania 7.
     """
-    return None
+    if (m < 1 or n < 1) or not isinstance(m, int) or not isinstance(n, int):
+        return None
+
+    matrix = np.zeros((m, n))
+    for i in range(m):
+        for j in range(n):
+            if i > j:
+                matrix[i][j] = i
+            else:
+                matrix[i][j] = j
+
+    return matrix
